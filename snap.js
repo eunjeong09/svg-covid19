fde2fc396567bfa3f0ffcf06de732899bffea934
eunjeong09 <ejko0911@gmail.com>
2021-04-13 19:53:27 +0900

    Add water animation on snap.js

--- snap.js
+++ snap.js
@@ -39,18 +39,25 @@ line1.attr({
 let cup = Snap('#cup');
 
 // 컵 안에 물 그리기
-// let cupWater = cup.path(d="M26,40 C 50 20, 65 10, 80 20 S 95 30, 110 20, 130 13, 147 25 L125, 150 45, 150z")
+let cupWater = cup.path(d="M26,40 C 50 20, 65 10, 80 20 S 95 30, 110 20, 130 13, 147 25 L125, 150 45, 150z")
 
-// cupWater.attr({
-//     stroke:"#E78484",
-//     fill:"#E78484",
-//     strokeWidth:3,
-//     strokeLinecap:"round"
-// });
+cupWater.attr({
+    stroke:"#E78484",
+    fill:"#E78484",
+    strokeWidth:3,
+    strokeLinecap:"round"
+});
 
 // water animation
+cupWater.animate({
+    d:"M24,20 C 50 40, 65 30, 80 13 S 95 25, 110 15, 130 40, 147 15 L125, 150 45, 150z;"
+    // d:"M26,40 C 50 20, 65 10, 80 20 S 95 30, 110 20, 130 13, 147 25 L125, 150 45, 150z;"
+}, 1000, mina.bounce)
+
 
 cup.polygon(20,5,150,5,125,150,45,150).attr({ fill: "none", stroke: "#000", strokeWidth:"3" });
+
+
 
 // cup.hover(function(){
 //     wave();
