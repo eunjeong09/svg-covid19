b789d3c5f6cae60b1e9643a862e2a28ce8fd8726
eunjeong09 <ejko0911@gmail.com>
2021-04-14 23:04:49 +0900

    Add snap path example 새로운 예제 적용, snap를 이용해서 svg path 그리기

--- snap.js
+++ snap.js
@@ -2,26 +2,26 @@
 let rectArea = Snap('#rectArea');
 let rect = rectArea.rect(10, 10, 50, 50);
 rect.attr({
-    fill:'#ff3f3f',
-    stroke:"#000",
-    strokeWidth:"3"
+    fill: '#ff3f3f',
+    stroke: "#000",
+    strokeWidth: "3"
 });
 
 // 원
 let circleArea = Snap('#circleArea');
 let circle = circleArea.circle(50, 50, 50);
 circle.attr({
-    fill:'#af54ff'
+    fill: '#af54ff'
 })
 
 // 구름
 let paper = Snap("#cloudArea");
 let water = paper.circle(50, 50, 30);
 // Lets put this small circle and another one into a group:
-let discs = paper.group(water, paper.circle(90, 50, 30), paper.circle(120, 50,30));
+let discs = paper.group(water, paper.circle(90, 50, 30), paper.circle(120, 50, 30));
 discs.attr({
-    fill:'#2870ff',
-    strokeWidth:2
+    fill: '#2870ff',
+    strokeWidth: 2
 });
 
 // discs.prependTo(paper);
@@ -30,32 +30,48 @@ discs.attr({
 let lineArea1 = Snap('#lineArea1');
 let line1 = lineArea1.line(50, 50, 100, 100);
 line1.attr({
-    fill:'#000',
-    strokeWidth:3,
-    stroke:"#000"
+    fill: '#000',
+    strokeWidth: 3,
+    stroke: "#000"
 });
 
 // 컵
 let cup = Snap('#cup');
 
 // 컵 안에 물 그리기
-let cupWater = cup.path(d="M26,40 C 50 20, 65 10, 80 20 S 95 30, 110 20, 130 13, 147 25 L125, 150 45, 150z")
+let cupWater = cup.path(d = "M26,40 C 50 20, 65 10, 80 20 S 95 30, 110 20, 130 13, 147 25 L125, 150 45, 150z")
 
 cupWater.attr({
-    stroke:"#E78484",
-    fill:"#E78484",
-    strokeWidth:3,
-    strokeLinecap:"round"
+    stroke: "#E78484",
+    fill: "#E78484",
+    strokeWidth: 3,
+    strokeLinecap: "round"
 });
 
 // water animation
 cupWater.animate({
-    d:"M24,20 C 50 40, 65 30, 80 13 S 95 25, 110 15, 130 40, 147 15 L125, 150 45, 150z;"
+    d: "M24,20 C 50 40, 65 30, 80 13 S 95 25, 110 15, 130 40, 147 15 L125, 150 45, 150z;",
     // d:"M26,40 C 50 20, 65 10, 80 20 S 95 30, 110 20, 130 13, 147 25 L125, 150 45, 150z;"
-}, 1000, mina.bounce)
+    // d:"M26,40 C 50 20, 65 10, 80 20 S 95 30, 110 20, 130 13, 147 25 L125, 150 45, 150z;"
+}, 1000, mina.linear)
 
 
-cup.polygon(20,5,150,5,125,150,45,150).attr({ fill: "none", stroke: "#000", strokeWidth:"3" });
+//snap로 path 그려내기..?
+// var s = Snap(cup.querySelector('svg')),
+var s = Snap(cup),
+    path = s.select('path'),
+    origPath = path.attr('d'),
+    route = cup.getAttribute('data-path-route'),
+    callback = function () {
+        path.animate({ 'path': origPath }, speed, mina.easeout);
+    };
+
+el.addEventListener('click', function () {
+    path.animate({ 'path': route }, speed, mina.easein, callback);
+});
+
+
+cup.polygon(20, 5, 150, 5, 125, 150, 45, 150).attr({ fill: "none", stroke: "#000", strokeWidth: "3" });
 
 
 
